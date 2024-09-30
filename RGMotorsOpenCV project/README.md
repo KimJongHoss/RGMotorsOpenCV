27b5209c9563ce288f4d001035fb9961f4c88842
RYUBoY <97417619+RYUBoY@users.noreply.github.com>
2024-09-30 12:34:06 +0900

    Delete RGMotorsOpenCV project directory

--- RGMotorsOpenCV project/README.md
+++
@@ -1,2 +0,0 @@
-# RGMotorsOpenCV
-세번째 프로젝트인 RG Motors의 OpenCV github입니다.
--- RGMotorsOpenCV project/src/test.py
+++
@@ -1,6 +0,0 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-
-plt.rcParams['font.family'] ='Malgun Gothic'
-plt.rcParams['axes.unicode_minus'] =False
